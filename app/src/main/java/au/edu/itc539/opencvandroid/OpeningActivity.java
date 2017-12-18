7a06279c3ae82e3c547569e376c22e465cf7900b
duane <duane.mcmahon@telstra.com>
2017-12-18 21:08:44 +1100

    clickable image impelmented ok for moto z play - clickable areas of opening image not scaling to other screen sizes/ devices. next step - handle successful object identification.

--- app/src/main/java/au/edu/itc539/opencvandroid/OpeningActivity.java
+++ app/src/main/java/au/edu/itc539/opencvandroid/OpeningActivity.java
@@ -1,29 +1,44 @@
 package au.edu.itc539.opencvandroid;
 
+import android.app.ActionBar;
 import android.app.Activity;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+
+import android.graphics.Rect;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageSwitcher;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ViewSwitcher;
 
 
-import java.util.ArrayList;
-import java.util.List;
-
-import at.lukle.clickableareasimage.ClickableArea;
-import at.lukle.clickableareasimage.ClickableAreasImage;
-import at.lukle.clickableareasimage.OnClickableAreaClickedListener;
-import uk.co.senab.photoview.PhotoViewAttacher;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
-public class OpeningActivity extends Activity implements OnClickableAreaClickedListener {
+public class OpeningActivity extends Activity {
 
     private static final String DEBUG_TAG = "OpeningActivity";
+    private float[] lastTouchDownXY = new float[2];
 
+    private Intent ii;
+    private Rect banana, orange;
+    private ImageSwitcher imageSwitcher;
+    private ImageView opening_view;
+    private TextView state;
+    final Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,25 +49,131 @@ public class OpeningActivity extends Activity implements OnClickableAreaClickedL
 
         setContentView(R.layout.show_opening);
 
+        state = findViewById(R.id.select);
 
-        ImageView image = findViewById(R.id.opening_background);
+        imageSwitcher = findViewById(R.id.opening_image);
 
-        // Create your image
-        ClickableAreasImage clickableAreasImage = new ClickableAreasImage(new PhotoViewAttacher(image), this);
+        imageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
 
-        // Initialize your clickable area list
-        List<ClickableArea> clickableAreas = new ArrayList<>();
+                opening_view = new ImageView(getApplicationContext());
 
-        // Define your clickable areas
-        // parameter values (pixels): (x coordinate, y coordinate, width, height) and assign an object to it
-        clickableAreas.add(new ClickableArea(58, 118, 72, 225, "banana"));
-        clickableAreas.add(new ClickableArea(249, 114, 106, 100, "orange"));
+                return opening_view;
 
-        // Set your clickable areas to the image
-        clickableAreasImage.setClickableAreas(clickableAreas);
+            }
+        });
+
+
+        imageSwitcher.setImageResource(R.drawable.theme_image);
+
+        opening_view.setOnTouchListener(touchListener);
+
+        opening_view.setOnClickListener(clickListener);
+
+        Animation in = AnimationUtils.loadAnimation(this, android.R.anim.fade_in);
+        imageSwitcher.setInAnimation(in);
+        //imageSwitcher.setOutAnimation(out);
+
+        // Define clickable areas
+
+        banana = new Rect(115, 450, 380, 920);
+
+        orange = new Rect(538, 462, 750, 643);
+
+
+
 
 
     }
+
+
+    // the purpose of the touch listener is just to store the touch X,Y coordinates
+    View.OnTouchListener touchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+
+            // save the X,Y coordinates
+            if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
+                lastTouchDownXY[0] = event.getX();
+                lastTouchDownXY[1] = event.getY();
+            }
+
+            // let the touch event pass on to whoever needs it
+            return false;
+        }
+    };
+
+    View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // retrieve the stored coordinates
+            float x = lastTouchDownXY[0];
+            float y = lastTouchDownXY[1];
+            Log.i("TAG", "onLongClick: x = " + x + ", y = " + y);
+
+            if (banana.contains((int) x, (int) y)) {
+
+                Log.i("TAG", "Banana Clicked");
+                // use the coordinates for whatever
+                imageSwitcher.setImageResource(R.drawable.banana_selected);
+
+                state.setText("banana");
+
+                handler.postDelayed(new Runnable() {
+
+                    @Override
+                    public void run() {
+
+
+                        ii = new Intent();
+
+                        ii.putExtra("fruit", "banana");
+
+                        ii.setClass(OpeningActivity.this, MainActivity.class);
+
+                        startActivity(ii);
+
+                        finish();
+
+                    }
+
+                }, 1000L);
+
+            }
+
+            if (orange.contains((int) x, (int) y)) {
+                // use the coordinates for whatever
+                imageSwitcher.setImageResource(R.drawable.orange_selected);
+
+                state.setText("orange");
+
+                handler.postDelayed(new Runnable() {
+
+                    @Override
+                    public void run() {
+
+                        ii = new Intent();
+
+                        ii.putExtra("fruit", "orange");
+
+                        ii.setClass(OpeningActivity.this, MainActivity.class);
+
+                        startActivity(ii);
+
+                        finish();
+
+                    }
+
+                }, 1000L);
+
+
+            }
+
+        }
+    };
+
+
 
     @Override
     protected void onDestroy() {
@@ -91,23 +212,5 @@ public class OpeningActivity extends Activity implements OnClickableAreaClickedL
     }
 
 
-    @Override
-    public void onClickableAreaTouched(Object item) {
 
-        switch ((String) item) {
-
-            case "banana":
-
-                startActivity(new Intent(this, BananaActivity.class));
-
-                finish();
-
-            case "orange":
-
-                startActivity(new Intent(this, OrangeActivity.class));
-
-                finish();
-
-        }
-    }
 }
