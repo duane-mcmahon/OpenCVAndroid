5a64c7893bcd09905d666047816a575386f085d4
duane <duane.mcmahon@telstra.com>
2017-12-24 21:34:12 +1100

    progress on successful detection of fruit to next fruit and finally then to multi-object select task.

---
+++ app/src/main/java/au/edu/itc539/opencvandroid/Detectable.java
@@ -0,0 +1,22 @@
+package au.edu.itc539.opencvandroid;
+
+import android.app.Application;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.Arrays.asList;
+
+/**
+ * Created by duane on 24/12/2017.
+ */
+
+public class Detectable extends Application {
+
+
+    List<String> detectables = new ArrayList<>(asList("orange", "banana"));
+
+
+}
